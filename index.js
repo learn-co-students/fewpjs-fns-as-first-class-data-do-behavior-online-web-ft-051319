e98c5b962476e55f24f7332f5d37235ed47366ef
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-16 15:10:22 +0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  let hoursmins=time.split(":")
+  let ans=parseInt(hoursmins[0],10)*60+parseInt(hoursmins[1],10)
+  if (ans < 12*60){
+    return "Good Morning"
+  } else if (12*60<=ans && ans<17*60){
+    return "Good Afternoon"
+  } else{
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = string
+}
